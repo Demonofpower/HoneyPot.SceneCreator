3ac16f282c6918fe448b7ebc8f1735d30d260cfc
Paranoia <9366819+Demonofpower@users.noreply.github.com>
2021-08-01 13:18:33 +0200

    added RelayCommand

---
+++ HoneyPot.SceneCreator/HoneyPot.SceneCreator.GUI/Helper/RelayCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace HoneyPot.SceneCreator.GUI.Helper
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action action;
+        private readonly Func<bool> canExecute;
+
+        public RelayCommand(Action action, Func<bool> canExecute = null)
+        {
+            this.action = action;
+            this.canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute == null || canExecute.Invoke();
+        }
+
+        public void Execute(object parameter)
+        {
+            action();
+        }
+    }
+}
