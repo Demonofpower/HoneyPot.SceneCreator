0057b3d5e39dff988c641a5e3a7166f3985a2e8c
Paranoia <9366819+Demonofpower@users.noreply.github.com>
2021-08-02 21:12:51 +0200

    added export btn

--- HoneyPot.SceneCreator/HoneyPot.SceneCreator.GUI/SceneWindowViewModel.cs
+++ HoneyPot.SceneCreator/HoneyPot.SceneCreator.GUI/SceneWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using System.Windows;
 using HoneyPot.SceneCreator.GUI.Helper;
 using HoneyPot.SceneCreator.GUI.SceneObjects;
 
@@ -10,13 +11,24 @@ namespace HoneyPot.SceneCreator.GUI
         private Step selectedStep;
 
         private RelayCommand newCommand;
+        private RelayCommand exportCommand;
 
-        public ObservableCollection<Step> Steps { get; set; } = new ObservableCollection<Step>() {new Step() {id = 0, type = StepType.DialogLine}, new Step() {id = 1, type = StepType.DialogLine}};
+        public ObservableCollection<Step> Steps { get; set; } = new ObservableCollection<Step>() {new Step() {id = 0, type = StepType.DialogLine, text = "abc"}, new Step() {id = 1, type = StepType.DialogLine}};
 
         public void OpenScene(Scene scene)
         {
             currScene = scene;
             Visible = true;
+        }
+        
+        private void NewStep()
+        {
+            Steps.Add(new Step());
+        }
+
+        private void Export()
+        {
+            MessageBox.Show("s");
         }
 
         public Step SelectedStep
@@ -27,16 +39,35 @@ namespace HoneyPot.SceneCreator.GUI
                 if (value == selectedStep) return;
                 selectedStep = value;
                 OnPropertyChanged();
+                OnPropertyChanged(nameof(Text));
+                OnPropertyChanged(nameof(AltGirlSpeaks));
             }
         }
-        
-        private void NewStep()
+
+        public string Text
         {
-            Steps.Add(new Step());
+            get => selectedStep?.text ?? "";
+            set
+            {
+                if (value == selectedStep.text) return;
+                selectedStep.text = value;
+                OnPropertyChanged();
+            }
         }
 
-        public RelayCommand NewCommand => newCommand ?? (newCommand = new RelayCommand(NewStep));
+        public bool AltGirlSpeaks
+        {
+            get => selectedStep?.altGirlSpeaks ?? false;
+            set
+            {
+                if (value == selectedStep.altGirlSpeaks) return;
+                selectedStep.altGirlSpeaks = value;
+                OnPropertyChanged();
+            }
+        }
 
-        
+
+        public RelayCommand NewCommand => newCommand ?? (newCommand = new RelayCommand(NewStep));
+        public RelayCommand ExportCommand => exportCommand ?? (exportCommand = new RelayCommand(Export));
     }
 }
